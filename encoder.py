6beb6ce3b4fd381987482a2926ae5e4cd36e08f8
HertogsSeppe <s.hertogs@student.tue.nl>
2023-12-06 14:01:58 +0100

    Refactored encoder

--- encoder.py
+++ encoder.py
@@ -1,4 +1,5 @@
-from util import file_to_binary_string, bits_to_bases, DNA_srand_to_file
+from util import file_to_binary_string, DNA_srand_to_file
+from config import codons
 
 
 class Encoder:
@@ -7,8 +8,48 @@ class Encoder:
 
     def encode(self, inputPath, outputPath):
         binaryString = file_to_binary_string(inputPath)
-        dnaStrand = bits_to_bases(binaryString, 3)
-        print(len(binaryString), len(dnaStrand), len(binaryString) / len(dnaStrand))
-        dnaStrand = bits_to_bases(binaryString, 4)
-        print(len(binaryString), len(dnaStrand), len(binaryString) / len(dnaStrand))
+        dnaStrand = self.bits_to_bases(binaryString, 4)
         DNA_srand_to_file(dnaStrand, outputPath)
+        # print(len(binaryString), len(dnaStrand), len(binaryString) / len(dnaStrand))
+
+    def bits_to_bases(self, bits, nr_codons=3):
+        if nr_codons == 3:
+            wl = 16
+        elif nr_codons == 4:
+            wl = 22
+        else:
+            print("ERROR: Invalid sequence")
+            return ""
+
+        zeroPad = (wl - len(bits) % wl) % wl
+        bits = bits + zeroPad * "0"
+
+        id_secuence = self.generate_index(id=0, padding=zeroPad)
+
+        strand = id_secuence
+
+        for i in range(len(bits) // wl):
+            word = int(bits[wl * i : wl * i + wl], 2)
+
+            strand += self.word_to_bases(word, nr_codons)
+
+        return strand
+
+    def generate_index(self, id=0, padding=0):
+        id_secuence = codons[padding % 47]
+
+        for i in range(2, -1, -1):
+            index = (id // (47**i)) % 47
+            id_secuence += codons[index]
+
+        return id_secuence
+
+    def word_to_bases(self, word, nr_codons):
+        strand = ""
+
+        for i in range(nr_codons - 1, -1, -1):
+            index = (word // (47**i)) % 47
+
+            strand += codons[index]
+
+        return strand
